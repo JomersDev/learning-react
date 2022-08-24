a13f5a2660106f208b1bbf899f105a23cc3fe582
JomersDev <james.mcgrory135@gmail.com>
2022-08-24 16:34:42 +1000

    Split code into components

--- scrimba-lessons/src/App.js
+++ scrimba-lessons/src/App.js
@@ -1,10 +1,16 @@
 import logo from './React-icon.png';
+import {style} from './style.css'
 
 function Header() {
   return (
     <header>
-      <nav>
-        <img src={logo} alt="react logo" width="40px" height="auto"/>
+      <nav className='nav-main'>
+          <img src={logo} alt="react logo" className='react-logo'/>
+          <ul className='nav-items'>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
       </nav>
     </header>
   )
@@ -12,14 +18,14 @@ function Header() {
 
 function MainContent() {
   return (
-    <>
-      <h1>Reasons I;m excited to learn react</h1>
+    <div className='main-content'>
+      <h1>Reasons I'm excited to learn react</h1>
       <ol>
         <li>Hireable skill</li>
         <li>In demand skill</li>
         <li>Interesting learning how websites are built</li>
       </ol>
-    </>
+    </div>
   )
 }
 
