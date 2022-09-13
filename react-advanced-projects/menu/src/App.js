8168fd3b80839c59499601941d2cb225294e2eb1
JomersDev <james.mcgrory135@gmail.com>
2022-09-13 11:50:37 +1000

    Added menu filtering and responsive resize

--- react-advanced-projects/menu/src/App.js
+++ react-advanced-projects/menu/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Menu from './Menu';
-import Nav from './Nav';
 import './App.css';
 import { menuData } from './data';
 
@@ -9,15 +8,38 @@ function App() {
   const[menuItems, setMenuItems] = React.useState(menuData)
 
   const menuElements = menuItems.map(item => {
-    return <Menu title={item.title} img={item.img} price={item.price} description={item.description}/>
+    return <Menu key={item.id} title={item.title} img={item.img} price={item.price} description={item.description}/>
   })
+
+  function showAll() {
+    setMenuItems(menuData)
+  }
+
+  function showBreakfast() {
+    setMenuItems(menuData.filter(item => item.category === "breakfast"))
+  }
+
+  function showLunch() {
+    setMenuItems(menuData.filter(item => item.category === "lunch"))
+  }
+
+  function showShakes() {
+    setMenuItems(menuData.filter(item => item.category === "shakes"))
+  }
 
   return (
     <div className="app">
         <h1>Our Menu</h1>
         <div className='underline'></div>
-        <Nav />
-        {menuElements}
+        <div className="nav-container">
+            <button className="btn" onClick={showAll}>All</button>
+            <button className="btn" onClick={showBreakfast}>Breakfast</button>
+            <button className="btn" onClick={showLunch}>Lunch</button>
+            <button className="btn" onClick={showShakes}>Shakes</button>
+        </div>
+        <div className='menu-grid'>
+          {menuElements}
+        </div>
     </div>
   );
 }
--- react-advanced-projects/menu/src/data.js
+++ react-advanced-projects/menu/src/data.js
@@ -3,6 +3,7 @@ export const menuData = [
         id: 1,
         img: "https://react-projects-5-menu.netlify.app/images/item-1.jpeg",
         title: "Buttermilk Pancakes",
+        category: "breakfast",
         price: "$12",
         description: "Deliciously buttery and fluffy with golden, crisp edges and an irresistible buttermilk flavour."
     },
@@ -10,6 +11,7 @@ export const menuData = [
         id: 2,
         img: "https://react-projects-5-menu.netlify.app/images/item-2.jpeg",
         title: "Diner Double",
+        category: "lunch",
         price: "$15",
         description: "Grilled Chicken breast, Mild cheese, Avocado spread, Fried egg, Caramelised onions, Aioli, French Fries"
     },
@@ -17,6 +19,7 @@ export const menuData = [
         id: 3,
         img: "https://react-projects-5-menu.netlify.app/images/item-3.jpeg",
         title: "Godzilla Milkshake",
+        category: "shakes",
         price: "$6",
         description: "Ice Cream, M&m’s, Chocolate, Nutella, Brownie"
     },
@@ -24,6 +27,7 @@ export const menuData = [
         id: 4,
         img: "https://react-projects-5-menu.netlify.app/images/item-4.jpeg",
         title: "Country delight",
+        category: "breakfast",
         price: "$10",
         description: "Sourdough toast with two free range eggs cooked your way"
     },
@@ -31,6 +35,7 @@ export const menuData = [
         id: 5,
         img: "https://react-projects-5-menu.netlify.app/images/item-6.jpeg",
         title: "Oreo Dream",
+        category: "shakes",
         price: "$7",
         description: "Ice Cream, Milk, Chocolate, Peanut Butter, Snickers"
     },
@@ -38,6 +43,7 @@ export const menuData = [
         id: 6,
         img: "https://react-projects-5-menu.netlify.app/images/item-7.jpeg",
         title: "Bacon Overflow",
+        category: "breakfast",
         price: "$12",
         description: "Smokey bacon, Fried free range egg, Bbq sauce "
     },
@@ -45,6 +51,7 @@ export const menuData = [
         id: 7,
         img: "https://react-projects-5-menu.netlify.app/images/item-8.jpeg",
         title: "American Classic",
+        category: "lunch",
         price: "$13",
         description: "Grass feed 180gr Angus beef patty, Onions Cheddar , Gherkins, Fresh tomato , Lettuce, Aioli, French Fries"
     },
@@ -52,6 +59,7 @@ export const menuData = [
         id: 8,
         img: "https://react-projects-5-menu.netlify.app/images/item-9.jpeg",
         title: "Quarantine Buddy",
+        category: "shakes",
         price: "$8",
         description: "Choice of Chocolate, Strawberry, Vanilla, Banana, Caramel"
     },
@@ -59,6 +67,7 @@ export const menuData = [
         id: 9,
         img: "https://react-projects-5-menu.netlify.app/images/item-5.jpeg",
         title: "Egg Attack",
+        category: "lunch",
         price: "$14",
         description: "Sourdough bread, Poached eggs, Potato Hash, Crispy kale, Herbed hollandaise, "
     },
